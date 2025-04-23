f9b41fbc0d4226addd5faa17bd2fcce9710b7dc4
pirakansa <pirakansa@users.noreply.github.com>
2025-04-23 12:54:00 +0000

    fix(events): refactor event queue initialization and remove unused pointer action functionality
    - Updated mutex initialization to use events_queue_mutex consistently.
    - Removed the pointer_action_dt enum and associated events_action function.
    - Simplified the initialize_ev_data, enqueue, and dequeue functions by directly manipulating queue_head and queue_cnt.

--- src/events/events.c
+++ src/events/events.c
@@ -5,59 +5,53 @@
 
 #define QUEUE_SIZE 10
 
-auto_init_mutex(events_pointer_mutex);
-
-typedef enum {
-    STACKEVENTS_ADD = 0,
-    STACKEVENTS_SUB,
-} pointer_action_dt;
+auto_init_mutex(events_queue_mutex);
 
 stackevents_dt stackevents[QUEUE_SIZE] = {STACKEVENTS_NONE};
 volatile int queue_head;
+volatile int queue_tail;
 volatile int queue_cnt;
 
 // Initialize the event queue
 int initialize_ev_data(){
+    mutex_enter_blocking(&events_queue_mutex);
     queue_head = 0;
+    queue_tail = 0;
     queue_cnt = 0;
+
+    mutex_exit(&events_queue_mutex);
     return 0;
 }
-
-// Perform an action on the event queue (add or remove)
-int events_action(pointer_action_dt act) {
-    mutex_enter_blocking(&events_pointer_mutex);
-
-    if (act == STACKEVENTS_ADD) {
-        queue_cnt++;
-    } else if (act == STACKEVENTS_SUB) {
-        queue_cnt--;
-        queue_head = (queue_head + 1) % QUEUE_SIZE;
-    }
-
-    mutex_exit(&events_pointer_mutex);
-    return 0;
-}
-
 
 stackevents_dt enqueue(stackevents_dt enq_data) {
+    mutex_enter_blocking(&events_queue_mutex);
+
     if (QUEUE_SIZE <= queue_cnt) {
+        mutex_exit(&events_queue_mutex);
         printf("Error: Queue overflow.\n");
         return STACKEVENTS_FULL;
     }
     
     stackevents[(queue_head + queue_cnt) % QUEUE_SIZE] = enq_data;
-    events_action(STACKEVENTS_ADD);
+    queue_cnt++;
+
+    mutex_exit(&events_queue_mutex);
     return enq_data;
 }
 
 stackevents_dt dequeue() {
+    mutex_enter_blocking(&events_queue_mutex);
+
     if (queue_cnt <= 0) {
         printf("Error: Queue underflow.\n");
+        mutex_exit(&events_queue_mutex);
         return STACKEVENTS_NONE;
     }
 
     stackevents_dt deq_data = stackevents[queue_head];
-    events_action(STACKEVENTS_SUB);
+    queue_cnt--;
+
+    mutex_exit(&events_queue_mutex);
     return deq_data;
 }
 
